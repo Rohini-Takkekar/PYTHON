05e431cffda6006a9b1196102d1ce681bd63ff4f
Rohini-Takkekar <rohinitakkekar24@gmail.com>
2022-01-17 16:51:51 +0530

    solar energy project

--- AI_ML_INTERNSHIP/SolarEnergy.py
+++ AI_ML_INTERNSHIP/SolarEnergy.py
@@ -10,4 +10,17 @@ print(data.head())
 print(data.info())
 print(data.describe())
 
+# extracting input and output variables
+x = data.drop(['Data', 'Time', 'TimeSunSet', 'Radiation'], axis=1)
+print(x.head())
+
+y= data['Radiation']
+print(y.head())
+
+predictors = ['Temperature','Pressure','Humidity','WindDirection(Degrees)','Speed']
+target = ["Radiation"]
+x= data[predictors].values
+y= data[target].values
+print(x)
+print(y)
 
